5488b7a7a7718ac7db1e1edabf0e7a81ef60c492
GabrielAttila <gabrieljmorenot@gmail.com>
2018-12-27 13:05:51 -0400

    3 - Configuración del entorno de pruebas

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+    use DatabaseTransactions;
     /**
      * A basic functional test example.
      *
@@ -13,7 +14,14 @@ class ExampleTest extends TestCase
      */
     public function testBasicExample()
     {
-        $this->visit('/')
-             ->see('Laravel');
+        $user = factory(\App\User::class)->create([
+            'name' => 'Gabriel Moreno',
+            'email' => 'gabrieljmroenot@gamil.com'
+        ]);
+
+        $this->actingAs($user, 'api')
+             ->visit('api/user')
+             ->see('Gabriel Moreno')
+             ->see('gabrieljmroenot@gamil.com');
     }
 }
