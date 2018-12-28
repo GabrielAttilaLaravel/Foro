d8d1e2fdc4ebe44821fb77e542df9d206f215a70
GabrielAttila <gabrieljmorenot@gmail.com>
2018-12-28 17:14:36 -0400

    Fix reference with references relation users posts

--- database/migrations/2018_12_28_203127_create_posts_table.php
+++ database/migrations/2018_12_28_203127_create_posts_table.php
@@ -21,7 +21,7 @@ class CreatePostsTable extends Migration
             $table->boolean('pending')->default(true);
 
             $table->unsignedInteger('user_id');
-            $table->foreign('user_id')->reference('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
 
             $table->timestamps();
         });
