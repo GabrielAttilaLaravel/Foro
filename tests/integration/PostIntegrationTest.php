8b383228ca6d3849265fd0a4dac378302976defd
GabrielAttila <gabrieljmorenot@gmail.com>
2019-01-03 21:28:33 -0400

    A user creates a post and subscribes to it

--- tests/integration/PostIntegrationTest.php
+++ tests/integration/PostIntegrationTest.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\User;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class PostIntegrationTest extends TestCase
@@ -14,5 +15,25 @@ class PostIntegrationTest extends TestCase
         ]);
 
         $this->assertSame( 'como-instalar-laravel', $post->fresh()->slug);
+    }
+
+    function test_a_user_creates_a_post_and_subscribes_to_it()
+    {
+        $user = factory(User::class)->create();
+
+        $post = $user->createPost([
+            'title' => 'titulo',
+            'content' => 'contenido',
+        ]);
+
+        $this->seeInDatabase('posts',[
+            'title' => $post->title,
+            'content' => $post->content,
+        ]);
+
+        $this->seeInDatabase('subscriptions', [
+            'user_id' => $user->id,
+            'post_id' => $post->id,
+        ]);
     }
 }
